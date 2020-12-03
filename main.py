a8f4505fcf2fa75cf75c3d661319e05005960ca4
ValeriyShashkin <shashkin_val@mail.ru>
2020-12-03 21:03:56 +0500

    incapsulation

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from _math import sigmoid
 from _test import test1, test2
 
 def backpropagation(x, Y, w):
-    current_Y = None
-
     for i in range(10000):
         current_Y = sigmoid(np.dot(x, w))
         error = Y - current_Y
         adjustment = np.dot(x.T, error * (current_Y * (1 - current_Y)))
         w += adjustment
-
-    return current_Y, w
+    return w
 
 train_x = np.array([
     [0, 1, 1, 0, 0, 0, 1],
@@ -28,9 +25,9 @@ print(train_x)
 print('Тренировочный набор ожидаемых выходных данных')
 print(train_Y)
 
-Y, weights = backpropagation(train_x, train_Y, weights)
+weights = backpropagation(train_x, train_Y, weights)
 print('Предсказание на тренировочном наборе данных')
-print(Y)
+print(sigmoid(np.dot(train_x, weights)))
 
-test1(w)
-test2(w)
+test1(weights)
+test2(weights)
